80d1a0353f0f4763d8eb085a83d82fcf9ab05dbe
SofiyanSheikh <sheikh.sofiyan@gmail.com>
2025-04-30 18:06:02 +0530

    Added for test

--- blocks/dynamicdata/dynamicdata.js
+++ blocks/dynamicdata/dynamicdata.js
@@ -8,7 +8,7 @@ export default async function decorate (block) {
 
 async function fetchData(block) {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-                            .then(res => res.clone().json)
+                            .then(res => res.clone().json())
                             block.append(response.title);
                             console.log(response);
 
