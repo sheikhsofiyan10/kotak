d827f2d4f03711bb83dea5a0faf32958b05fce7b
SofiyanSheikh <sheikh.sofiyan@gmail.com>
2025-04-30 18:06:39 +0530

    Added for test

--- blocks/dynamicdata/dynamicdata.js
+++ blocks/dynamicdata/dynamicdata.js
@@ -7,7 +7,7 @@ export default async function decorate (block) {
 }
 
 async function fetchData(block) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const response = await fetch('https://publish-p110016-e1074395.adobeaemcloud.com/graphql/execute.json/santanderbank/OpenbankTeaserList')
                             .then(res => res.clone().json())
                             block.append(response.title);
                             console.log(response);
