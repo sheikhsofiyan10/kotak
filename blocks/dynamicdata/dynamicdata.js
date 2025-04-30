35a55ed4ac5a594469e5f2c64209b973404f6cda
SofiyanSheikh <sheikh.sofiyan@gmail.com>
2025-04-30 21:11:52 +0530

    Test

--- blocks/dynamicdata/dynamicdata.js
+++ blocks/dynamicdata/dynamicdata.js
@@ -10,7 +10,7 @@ async function fetchData(block) {
   const response = await fetch(
   'https://publish-p110016-e1074395.adobeaemcloud.com/graphql/execute.json/santanderbank/OpenbankTeaserList',
   {mode:"no-cors"}
-  ).then(res => res.clone().json());
+  );
   block.append(response.title);
   console.log(response);
 
