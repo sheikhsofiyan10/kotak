6d947ac723c17f1d6f5aeca3b14bf014f222759e
SofiyanSheikh <sheikh.sofiyan@gmail.com>
2025-05-11 14:12:54 +0530

    Added fix for carousel block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,33 +1,36 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-    // Create the main carousel div
-    // Add class to the carousel wrapper
     document.querySelector(".carousel-wrapper").classList.add("owl-theme");
+        document.querySelector(".carousel.block").classList.add("owl-theme");
+        let firstDivs = document.querySelectorAll(".carousel.block > div:nth-of-type(1) > div");
+        firstDivs[0].classList.add("carousel-image");
+        firstDivs[1].classList.add("carousel-content");
+        document.querySelector(".carousel-image picture").classList.add("carousel-image");
+        let firstParagraphs = document.querySelectorAll(".carousel-content p");
+        firstParagraphs[0].classList.add("main-text");
+        firstParagraphs[1].classList.add("sub-text");
+        firstParagraphs[2].classList.add("sub-text");
+        const buttonContainers = document.querySelectorAll(".carousel-content .button-container");
+        if (buttonContainers.length >= 2) {
+            const combinedDiv = document.createElement("div");
+            combinedDiv.classList.add("button-wrapper-combined");
+            // Insert the combined div before the first button container
+            buttonContainers[0].parentNode.insertBefore(combinedDiv, buttonContainers[0]);
+            // Move both button containers into the combined div
+            combinedDiv.appendChild(buttonContainers[0]);
+            combinedDiv.appendChild(buttonContainers[1]);
+        }
 
-    document.querySelector(".carousel.block").classList.add("owl-theme");
-
-    let firstDivs = document.querySelectorAll(".carousel.block > div:nth-of-type(1) > div");
-    firstDivs[0].classList.add("carousel-image");
-    firstDivs[1].classList.add("carousel-content");
-
-    document.querySelector(".carousel-image picture").classList.add("carousel-image");
-
-    let firstParagraphs = document.querySelectorAll(".carousel-content p");
-    firstParagraphs[0].classList.add("main-text");
-    firstParagraphs[1].classList.add("sub-text");
-    firstParagraphs[2].classList.add("carousel-text");
-    // firstParagraphs[3].classList.add("button-container");
-
-
-    let secondDivs = document.querySelectorAll(".carousel.block > div:nth-of-type(2) > div");
-    secondDivs[0].classList.add("carousel-image");
-    secondDivs[1].classList.add("carousel-content", "carousel-content2");
-
-    savingsButton = secondDivs[1].querySelector("a.button");
-    if (savingsButton) {
-        savingsButton.classList.add("savings-btn");
-    }
+        let learnMoreLink = document.querySelector(".carousel-content p:nth-of-type(3) a");
+        if (learnMoreLink) {
+            learnMoreLink.classList.add("learn-more-link");
+        }
+        let secondDivs = document.querySelectorAll(".carousel.block > div:nth-of-type(2) > div");
+        if (secondDivs.length > 0) {
+            secondDivs[0].classList.add("carousel-image");
+            secondDivs[1].classList.add("carousel-content", "carousel-content2");
+        }
 }
 
 
