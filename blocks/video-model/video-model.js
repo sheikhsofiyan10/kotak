99d583c182f7badc37dade4b53ec1c5283daafc5
SofiyanSheikh <sheikh.sofiyan@gmail.com>
2025-05-12 13:58:59 +0530

    Added video model block

--- blocks/video-model/video-model.js
+++ blocks/video-model/video-model.js
@@ -1,3 +1,31 @@
-export default async function decorate (block) {
-     console.log('blockkkkkkkkkkk', block);
-    }
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+
+  console.log('block', block);
+
+
+ const videoId = block.querySelector('p').innerHTML;
+ console.log(videoId,"videoidd")
+
+ block.textContent = "";
+
+  let modal = `
+    <div class=modal fade id=videoModal-${videoId} tabindex=-1 role=dialog aria-labelledby=videoModalLabel aria-hidden=true>
+     <div class=modal-dialog modal-lg modal-dialog-centered role=document>
+     <div class=modal-content>
+     <div class=modal-body>
+      <button type=button class=close data-dismiss=modal aria-label=Close onclick=stopVideo()>
+        &times;
+        </button>
+     <div class=embed-responsive embed-responsive-16by9>
+     <iframe id=videoFrame class=embed-responsive-item src=https://www.youtube.com/embed/${videoId} allowfullscreen height=300px></iframe>
+     </div>
+     </div>
+     </div>
+     </div>
+    </div>`;
+
+  block.innerHTML = modal;
+}
