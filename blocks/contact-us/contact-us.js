a8597447f02fe150abe0f1a5a761c11f3591a5aa
Ashish Lohia <ashishlohia@Ashishs-MacBook-Air.local>
2025-05-14 15:58:16 +0530

    page responsive done

--- blocks/contact-us/contact-us.js
+++ blocks/contact-us/contact-us.js
@@ -1,7 +1,16 @@
-import {
-  createOptimizedPicture
-} from '../../scripts/aem.js';
+import { createOptimizedPicture } from "../../scripts/aem.js";
 
-export default async function decorate (block) {
+export default async function decorate(block) {
+  console.log(block, "contact-us");
 
+  [...block.children].forEach((row) => {
+    console.log(row);
+    row.className = "contact-us-card";
+
+    [...row.children].forEach((card) => {
+      if (card.children.length === 1 && card.querySelector("picture"))
+        card.className = "card-image";
+      else card.className = "card-body";
+    });
+  });
 }
