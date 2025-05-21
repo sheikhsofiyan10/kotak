184c217c47149af5c6dd1571baeca4e8d3ba40f1
Ashish Lohia <ashishlohia@Ashishs-MacBook-Air.local>
2025-05-21 11:55:29 +0530

    offers carousel added

---
+++ blocks/offers-carousel/offers-carousel.js
@@ -0,0 +1,42 @@
+const createCardCarousel = async () => {
+  const res = await fetch("/data/mock-offers.json");
+  const cardsData = await res.json();
+
+  const carouselContainer = document.createElement("div");
+  carouselContainer.className = "owl-carousel";
+
+  // Loop through each card data
+  cardsData?.mostPopularList.forEach((card) => {
+    const cardEl = document.createElement("div");
+    cardEl.className = "card";
+
+    cardEl.innerHTML = `
+         <div class="card-image">
+           <img src="${card.thumbnailImage}" alt="${card.offerTitleModified}" loading="lazy">
+         </div>
+         <div class="card-body">
+           <h4>${card.offerTitleModified}</h4>
+           <p>${card.description}</p>
+           <p class="valid-info"> Valid Till: ${card.endDate}</p>
+           <p>
+             <a href="#" class="link-button" target="_blank" rel="noopener">Read more</a>
+           </p>
+         </div>
+       `;
+
+    carouselContainer.appendChild(cardEl);
+  });
+
+  return carouselContainer;
+};
+
+export default async function decorate(block) {
+  block.innerHTML = "";
+
+  try {
+    const carouselContainer = await createCardCarousel();
+    block.appendChild(carouselContainer);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,45 +1,67 @@
 // add delayed functionality here
 
-
-
 $(".carausel-tabs .owl-carousel").owlCarousel({
-      items: 4,
-      loop: true,
-      margin: 10,
-      nav: true,
-      autoplay: false,
-      dots:false,
-      autoplayTimeout: 3000,
-      responsive: {
-        0:{
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 4
-        }
+  items: 4,
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: false,
+  dots: false,
+  autoplayTimeout: 3000,
+  responsive: {
+    0: {
+      items: 1,
     },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
 
-    });
+$(".owl-carousel.card-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: false,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
 
-    $(".owl-carousel.card-carousel").owlCarousel({
-            loop: true,
-            margin: 10,
-            autoplay: false,
-            autoplayTimeout: 3000,
-            autoplayHoverPause: false,
-            dots: true,
-            responsive: {
-                0:{
-                    items:2
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 5
-                }
-            },
-        });
+$(".offers-carousel .owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: true,
+  autoplay: false,
+  dots: false,
+  responsiveClass: true,
+  autoplayTimeout: 3000,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: false,
+    },
+  },
+});
